85b7d2a570ea6c27b1c511e68f73a487e6c9d494
William Cavalcante <williambreno9@gmail.com>
2023-04-20 11:03:08 -0300

    fix espaco vazio

--- mirrorfashion-frontend/js/coneccao-servidor.js
+++ mirrorfashion-frontend/js/coneccao-servidor.js
@@ -1,37 +1,38 @@
-const imagesUrl = (page) => `https://picsum.photos/v2/list?limit=8&page=${page}`;
+const imagesUrl = (page) => `https://picsum.photos/v2/list?limit=9&page=${page}`;
 
 const newPainel = (selector) => {
+  const parent = document.querySelector(selector);
+  const list = document.createElement("ol");
+  parent.appendChild(list);
+
   let p = {
     page: 1,
-    element: document.querySelector(selector)
+    list: list 
   };
 
   p.render = () => 
-    fetch(imagesUrl(p.page)).then(response => response.json())
-      .then(data => {
-        const l = document.createElement("ol");
+    fetch(imagesUrl(p.page))
+      .then(response => response.json())
+      .then(data => data.forEach(imagem => {
+        const item = document.createElement("li");
 
-        data.forEach(imagem => {
-          const item = document.createElement("li");
-          item.innerHTML = 
-            `<a>
-              <figure>
-                <img class="imagem">
-                <figcaption></figcaption>
-              </figure>
-            </a>`
+        item.innerHTML = 
+          `<a>
+            <figure>
+              <img class="imagem">
+              <figcaption></figcaption>
+            </figure>
+          </a>`
 
-          item.querySelector("a").href = "produto.html";
-          item.querySelector("img").src = imagem.download_url;
-          item.querySelector("figcaption").innerHTML = imagem.author;
+        item.querySelector("a").href = "produto.html";
+        item.querySelector("img").src = imagem.download_url;
+        item.querySelector("figcaption").innerHTML = imagem.author;
 
-          l.appendChild(item);
-        });
+        p.list.appendChild(item);
+      }))
+      .catch(error => console.log(error));
 
-        p.element.appendChild(l);
-      }).catch(error => console.log(error));
-
-  p.element.querySelector("button").addEventListener("click", () => {
+  parent.querySelector("button").addEventListener("click", () => {
     p.page += 1;
     p.render();
   });
