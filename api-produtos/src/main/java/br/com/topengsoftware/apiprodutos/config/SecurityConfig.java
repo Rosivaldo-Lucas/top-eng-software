8208a35f68c4db65732383bfca96c9426f3a1589
Mqs3 <miqueiasjp@gmail.com>
2023-05-18 20:37:50 -0300

    admin 403

--- api-produtos/src/main/java/br/com/topengsoftware/apiprodutos/config/SecurityConfig.java
+++ api-produtos/src/main/java/br/com/topengsoftware/apiprodutos/config/SecurityConfig.java
@@ -2,10 +2,15 @@ package br.com.topengsoftware.apiprodutos.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,12 +18,27 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorized -> authorized.anyRequest().permitAll())
+        http.authorizeHttpRequests(authorized -> authorized
+                        .requestMatchers(HttpMethod.GET, "/produtos").permitAll()
+                        .requestMatchers(HttpMethod.DELETE, "/produtos/*").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/produtos").hasRole("ADMIN")
+                        .anyRequest().permitAll())
                 .csrf().disable();
 
         return http.build();
     }
 
+    @Bean
+    public UserDetailsService users() {
+        // The builder will ensure the passwords are encoded before saving in memory
+        User.UserBuilder users = User.builder();
+        UserDetails admin = users
+                .username("admin")
+                .password("123mudar")
+                .roles("USER", "ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(admin);
+    }
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
--- api-produtos/src/main/java/br/com/topengsoftware/apiprodutos/domain/usuario/UsuarioController.java
+++ api-produtos/src/main/java/br/com/topengsoftware/apiprodutos/domain/usuario/UsuarioController.java
@@ -4,10 +4,7 @@ import br.com.topengsoftware.apiprodutos.domain.usuario.dto.UsuarioInput;
 import br.com.topengsoftware.apiprodutos.domain.usuario.dto.UsuarioOutput;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/usuarios")
