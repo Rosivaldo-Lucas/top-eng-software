d249f4b173d7559f321732288cc168f4620a6ae1
William Cavalcante <williambreno9@gmail.com>
2023-05-18 22:15:31 -0300

    fix api docs

--- api-produtos/src/main/java/br/com/topengsoftware/apiprodutos/config/SecurityConfig.java
+++ api-produtos/src/main/java/br/com/topengsoftware/apiprodutos/config/SecurityConfig.java
@@ -5,10 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,22 +13,18 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static java.util.EnumSet.allOf;
-import static org.springframework.security.authorization.AuthorityReactiveAuthorizationManager.hasAuthority;
-import static org.springframework.security.authorization.AuthorityReactiveAuthorizationManager.hasRole;
-
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     @Bean
-    public SecurityFilterChain web(HttpSecurity http) throws Exception {
-        GrantedAuthority authority = new SimpleGrantedAuthority("myAuthority");
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorized -> authorized
-                        .requestMatchers(HttpMethod.GET, "/produtos").permitAll()
-                        .requestMatchers(HttpMethod.DELETE, "/produtos/*").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.POST, "/produtos").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/produtos/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/produtos/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/produtos/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/usuarios").hasRole("ADMIN")
-                        .anyRequest().permitAll())
+                        .requestMatchers("/v3/api-docs/api").permitAll()
+                        .anyRequest().denyAll())
                 .httpBasic()
                 .and()
                 .csrf().disable();
@@ -41,13 +34,13 @@ public class SecurityConfig {
 
     @Bean
     public UserDetailsService users() {
-
         UserDetails admin = User.withUsername("admin")
                 .password(passwordEncoder().encode("123mudar"))
                 .roles("USER", "ADMIN")
                 .build();
         return new InMemoryUserDetailsManager(admin);
     }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
