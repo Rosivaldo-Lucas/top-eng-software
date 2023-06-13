43a02ddd5a555125c57f7366d288516d823e6ef7
Mqs3 <miqueiasjp@gmail.com>
2023-06-13 18:12:36 -0300

    ligando front com back sem autenticação

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
-function Input({texto, type='text'}) {
+function Input({texto, type='text', name, value, handleChange}) {
   return (
       <label>
           <input required placeholder="" 
-          type={type} className="input" />
+          type={type} className="input"
+          name={name} value={value} onChange={handleChange} />
           <span>{texto}</span>
         </label>
   );
